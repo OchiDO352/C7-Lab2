4b11cb7e4026fc33bd86468b335fdb4798ea24d3
OchiDO352 <jimmy3261237@gmail.com>
2022-02-25 15:33:53 +0700

    C7-Lab2

---
+++ Assets/Changcolors.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Changcolors : MonoBehaviour
+{
+  public void getRed(){
+         GetComponent<Renderer>().material.color = Color.red; 
+    }
+    public void getGreen(){
+        GetComponent<Renderer>().material.color = Color.green; 
+    }
+    public void getBlue(){
+        GetComponent<Renderer>().material.color = Color.blue; 
+    }
+}
---
+++ Assets/ChangeAnimator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimator : MonoBehaviour
+{
+   public void getAmimatorScream(){
+       this.GetComponent<Animator>().Play("Scream");
+   }
+
+   public void getAmimatorWalk(){
+       this.GetComponent<Animator>().Play("Walk");
+   }
+}
